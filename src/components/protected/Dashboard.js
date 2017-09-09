9ce134a7ea50679be1238aeb29e9b159b5806f76
Ivana <ivana.he.lili@gmail.com>
2017-09-09 15:30:30 -0700

    selectable timeslot table

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react'
-import {GridList, GridTile} from 'material-ui/GridList';
-import Subheader from 'material-ui/Subheader';
-import IconButton from 'material-ui/IconButton';
-// import CheckBox from 'material-ui/svg-icons/toggle/check-box-outline-blank';
-import Checkbox from 'material-ui/Checkbox';
+import $ from 'jquery'
 import {
   Table,
   TableBody,
@@ -13,41 +9,39 @@ import {
   TableRowColumn,
 } from 'material-ui/Table';
 
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-    width: 700,
-    height: 600,
-    overflowY: 'auto',
-  },
-};
-
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
 export default class Dashboard extends Component {
+  /** #E59500 (yellow) for unselected timeslot/available
+  #840032 (red) for selected timeslot/unavailable
+  **/
   state = {
     tilesData: [],
     busySlots: []
   }
 
+  onCellClick = this.onCellClick.bind(this);
 
   onCellClick(row,col) {
-    console.log('row',row);
-    console.log('col',col);
+    this.state.busySlots.push([row, col]);
+
+    if (this.state[row+','+col] === "#E59500") {
+      this.setState ({
+       [row+','+col] : "#840032"
+      })
+    } else {
+      this.setState ({
+        [row+','+col] : "#E59500"
+      })
+    }
   }
 
   componentWillMount() {
     let count = 0;
     let hour = 8;
     let hourMax = 9;
-    let day = 0;
 
     for (let i = 0; i < 9; i++) { 
-    
         hour = hourMax;
 
         if (hour === 12) {
@@ -56,11 +50,14 @@ export default class Dashboard extends Component {
           hourMax++; 
         }
           this.state.tilesData.push({ key: i, title: hour + '-' + hourMax });
-      
-        count++;
+          count++;
     }
-    console.log(this.state.tilesData);
-    console.log(this.state.busySlots);
+
+    for (let j = 0; j < 9; j++) {
+      for (let k = 0; k < 5; k++) {
+        this.state[j+','+k] = "#E59500";
+      }
+    }
   }
   
   render () {
@@ -74,13 +71,14 @@ export default class Dashboard extends Component {
           </TableRow>
         </TableHeader>
         <TableBody displayRowCheckbox={false}>
-         {this.state.tilesData.map((tile, index) => (
+         {
+          this.state.tilesData.map((tile, index) => (
             <TableRow key={index}>
-                <TableRowColumn onCellClick={this.onCellClick}><span>{tile.title}</span></TableRowColumn>
-                <TableRowColumn onCellClick={this.onCellClick}>{tile.title}</TableRowColumn>
-                <TableRowColumn onCellClick={this.onCellClick}>{tile.title}</TableRowColumn>
-                <TableRowColumn onCellClick={this.onCellClick}>{tile.title}</TableRowColumn>
-                <TableRowColumn onCellClick={this.onCellClick}>{tile.title}</TableRowColumn>
+                <TableRowColumn key={index+','+'0'} style={{color: "white", backgroundColor: this.state[index+','+'0']}} onCellClick={this.onCellClick}>{tile.title}</TableRowColumn> 
+                <TableRowColumn key={index+','+'0'} style={{color: "white", backgroundColor: this.state[index+','+'1']}} onCellClick={this.onCellClick}>{tile.title}</TableRowColumn> 
+                <TableRowColumn key={index+','+'0'} style={{color: "white", backgroundColor: this.state[index+','+'2']}} onCellClick={this.onCellClick}>{tile.title}</TableRowColumn> 
+                <TableRowColumn key={index+','+'0'} style={{color: "white", backgroundColor: this.state[index+','+'3']}} onCellClick={this.onCellClick}>{tile.title}</TableRowColumn> 
+                <TableRowColumn key={index+','+'0'} style={{color: "white", backgroundColor: this.state[index+','+'4']}} onCellClick={this.onCellClick}>{tile.title}</TableRowColumn>        
             </TableRow>
           ))}
         </TableBody>
