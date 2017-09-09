58ae6928dc6d0d9912fffd79772c4383abe5bb14
Ivana <ivana.he.lili@gmail.com>
2017-09-08 23:57:07 -0700

    add material ui theme wrapper

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './components';
 import './index.css';
 
+const Wrap = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <App />,
+  <Wrap />,
   document.getElementById('root')
 );
