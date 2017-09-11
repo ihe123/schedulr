6208f52d7d4a97534ab1891b51feb6d4336229ae
Ivana He <ivana.he.lili@gmail.com>
2017-09-11 00:26:19 -0700

    submit button css

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,15 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyDHL6JFTyBcaV60WpE4yXfeO0aZbzA9Xbk",
-  authDomain: "practice-auth.firebaseapp.com",
-  databaseURL: "https://practice-auth.firebaseio.com",
-}
+  apiKey: "AIzaSyAtsLninKgLQ_mmUSv32AWPuWILsTxM00w",
+  authDomain: "practicum-timeslot-scheduler.firebaseapp.com",
+  databaseURL: "https://practicum-timeslot-scheduler.firebaseio.com",
+  projectId: "practicum-timeslot-scheduler",
+  storageBucket: "",
+  messagingSenderId: "266432819239"
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 
 export const ref = firebase.database().ref()
 export const firebaseAuth = firebase.auth
